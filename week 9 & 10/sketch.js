83c0747cc8b55cbc7ac99f029988ac2e580c8cd5
Natalie Verploegen <snoopy.verp@gmail.com>
2021-03-26 11:42:47 -0600

    bob

---
+++ week 9 & 10/sketch.js
@@ -0,0 +1,135 @@
+function setup()
+        {
+            createCanvas (500,600);
+        }
+        function draw()
+        {
+            background(90, 60, 450);
+
+            //top head hair
+            fill('brown');
+            stroke('brown');
+            ellipse(250,330,170,160);
+
+            //neck
+            stroke('black')
+            fill(270, 200, 70);
+            rect(225,295,50,60);
+            
+
+            //head
+            stroke('black')
+            fill(270, 200, 70);
+            circle(250,200,200);
+            
+
+            //white of eyes
+            fill('white');
+            ellipse(285,200,40,25);
+            ellipse(210,200,40,25);
+
+            //iris
+            fill(color(0, 0, 255));
+            circle(285,200,25);
+            circle(210,200,25);
+
+            //pupil
+            fill('#222222');
+            circle(285,200,12);
+            circle(210,200,12);
+            
+            //eyebrows
+            fill('brown');
+            rect(270,180,25,5);
+            rect(200,180,25,5);            
+
+            //lips
+            line(230, 260, 265, 260);
+            curve(240, 260, 240, 270, 255, 270, 255, 260)
+
+            //shirt body
+            fill('purple');
+            stroke('purple');
+            rect(135,350,220,250);
+
+            //arms and hand
+            fill(270, 200, 70);
+            stroke(color('black'));
+            ellipse(135,445,40,190);
+            ellipse(355,445,40,190);
+            ellipse(225,530,200,40);
+            ellipse(355,600,40,190);
+            circle(330, 530, 47);
+            ellipse(343,507,40,14);
+            ellipse(360,518,30,15);
+            ellipse(360,527,30,15);
+            ellipse(360,535,30,15);
+            ellipse(360,544,30,13);
+
+            //ridge of nose in points
+            point(245, 216);
+            point(245, 218);
+            point(245, 220);
+            point(246, 222);
+            point(246, 224);
+            point(246, 226);
+            point(245, 228);
+            point(245, 230);
+            point(245, 232);
+            point(244, 234);
+
+            //bottom of nose in points
+            point(240, 240);
+            point(242, 240);
+            point(244, 241);
+            point(246, 241);
+            point(248, 240);
+            point(250, 240);
+            
+            //shirt sleeves
+            fill('purple');
+            stroke('purple');
+            triangle(135, 345, 160, 380, 110, 380);
+            triangle(355, 345, 330, 380, 380, 380);
+
+            //hair part 1
+            fill('brown');
+            stroke('brown');
+            ellipse(250,130,150,60);
+            ellipse(170,260,37,240);
+            ellipse(330,260,37,240);
+            ellipse(160,240,37,220);
+            ellipse(340,240,37,220);
+
+            //hair part 2
+            ellipse(180,140,50,40);
+            ellipse(180,150,60,45);
+            ellipse(320,140,50,40);
+            ellipse(320,150,60,45);
+            ellipse(300,145,60,45);
+            ellipse(315,160,45,60);
+            ellipse(325,180,45,60);
+
+            //hair part 3
+            ellipse(190,340,17,90);
+            ellipse(207,340,17,83);
+            ellipse(219,340,17,86);
+            ellipse(310,340,17,90);
+            ellipse(290,340,17,83);
+            ellipse(275,340,17,86);
+
+            //Title
+            fill('black');
+            stroke('black');
+            textSize(32);
+            text('Natalie Verploegen', 250, 50);
+            textSize(20);
+            text('as shapes', 250, 80);
+            textAlign(CENTER);
+            textFont('Times New Romans');
+
+            //Signature
+            textSize(20);
+            text('Natalie Verploegen', 80, 590);
+            textFont('Helvetica');
+        }
